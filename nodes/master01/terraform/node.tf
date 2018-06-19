77611a908d2eb38b2cb1bdb2ab26e6454febd68c
Piotr Grzesik <pj.grzesik@gmail.com>
2018-06-19 08:43:29 +0200

    Add more configuration options

--- nodes/master01/terraform/node.tf
+++ nodes/master01/terraform/node.tf
@@ -1,46 +1,61 @@
 resource "digitalocean_volume" "test_volume" {
-  region      = "lon1"
-  name        = "test-volume"
-  size        = 20
+  region      = "lon1"           # TODO: Make it configurable
+  name        = "test-volume"    # TODO: c
+  size        = 20               # TODO: c
+  description = "Example volume" # TODO: c
 }
 
 resource "digitalocean_droplet" "test_server" {
-  image = "30970148"
-  name = "test-server"
-  region = "lon1"
-  size = "s-1vcpu-2gb"
-  ssh_keys = [21715247]
-  volume_ids = ["${digitalocean_volume.test_volume.id}"]
+  image              = "30970148"                                # TODO: c
+  name               = "test-server"                             # TODO: c
+  region             = "lon1"                                    # TODO: c
+  size               = "s-1vcpu-2gb"                             # TODO: c
+  backups            = false                                     # TODO: c
+  monitoring         = false                                     # TODO: c
+  ipv6               = false                                     # TODO: c
+  private_networking = false                                     # TODO: c
+  ssh_keys           = [21715247]                                # TODO: c
+  volume_ids         = ["${digitalocean_volume.test_volume.id}"]
 }
 
 resource "digitalocean_firewall" "test_firewall" {
-  name = "test-firewall"
+  name = "test-firewall" # TODO: c
 
   droplet_ids = ["${digitalocean_droplet.test_server.id}"]
 
   inbound_rule = [
     {
-      protocol           = "tcp"
-      port_range         = "22"
-      source_addresses   = ["0.0.0.0/0", "::/0"]
+      protocol         = "tcp"
+      port_range       = "22"
+      source_addresses = ["0.0.0.0/0", "::/0"]
     },
     {
-      protocol           = "tcp"
-      port_range         = "80"
-      source_addresses   = ["0.0.0.0/0", "::/0"]
+      protocol         = "tcp"
+      port_range       = "80"
+      source_addresses = ["0.0.0.0/0", "::/0"]
     },
     {
-      protocol           = "tcp"
-      port_range         = "443"
-      source_addresses   = ["0.0.0.0/0", "::/0"]
+      protocol         = "tcp"
+      port_range       = "443"
+      source_addresses = ["0.0.0.0/0", "::/0"]
     },
   ]
 
   outbound_rule = [
     {
-      protocol                = "tcp"
-      port_range              = "80"
-      destination_addresses   = ["0.0.0.0/0", "::/0"]
+      protocol              = "icmp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "tcp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "udp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
     }
   ]
 }
